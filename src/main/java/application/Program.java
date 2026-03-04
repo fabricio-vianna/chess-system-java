3917fd19e0890eddad82ac704d15fe9b623026ae
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-03-04 11:40:44 -0300

    Starting to implement Board and Piece

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
-import entities.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(1, 3);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
 
     }
 }
